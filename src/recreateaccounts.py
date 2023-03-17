8e1acdcd164eb8e35911e80f964179c9ec806c12
Pieter Meulenhoff <pieter@meulenhoff.org>
2023-03-17 22:53:27 +0000

    Added tool to recreate accounts

---
+++ src/recreateaccounts.py
@@ -0,0 +1,26 @@
+import userhelper
+import settings
+import json
+import redis
+import asyncio
+from userhelper import User
+
+settings.init()
+
+
+for userkey in settings.rds.scan_iter("user:*"):
+    userdata = settings.rds.hget(userkey,"userdata")
+
+    if userdata is not None:
+        userdata = json.loads(userdata)
+
+        tgusername = userdata['telegram_username']
+        tguserid = userdata['telegram_userid']
+
+        print(tgusername,tguserid)
+
+        settings.rds.hdel(userkey,"userdata")
+
+        user = asyncio.run(userhelper.get_or_create_user(tguserid, tgusername))
+
+        print(user.lnurlp)
